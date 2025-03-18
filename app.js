e936f767b5642183ddee9bbb55ad71431416019b
jothika-nerdshub <jothikashankar@nerdshub.co.in>
2025-03-18 12:41:28 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+xconst express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Jenkins!');
+});
+
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
+});
